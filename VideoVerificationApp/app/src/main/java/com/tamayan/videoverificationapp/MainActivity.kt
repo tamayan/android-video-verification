3852a7ae1fe7f6a5ea0b3144639e38bf500dfb37
Yuki Kitagawa <kitagawa@tam-bourine.co.jp>
2018-07-31 18:13:48 +0900

    Implementation movie playback

--- VideoVerificationApp/app/src/main/java/com/tamayan/videoverificationapp/MainActivity.kt
+++ VideoVerificationApp/app/src/main/java/com/tamayan/videoverificationapp/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.tamayan.videoverificationapp
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.google.android.youtube.player.YouTubeStandalonePlayer
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubeThumbnailLoader
+import com.google.android.youtube.player.YouTubeThumbnailView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : YouTubeBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = YouTubeStandalonePlayer.createVideoIntent(this, BuildConfig.API_KEY, BuildConfig.VIDEO_ID)
-        startActivity(intent)
+        youTubePlayerView
+                .initialize(BuildConfig.API_KEY, object : YouTubePlayer.OnInitializedListener {
+                    override fun onInitializationSuccess(provider: YouTubePlayer.Provider, player: YouTubePlayer, wasRestored: Boolean) {
+                        player.loadVideo(BuildConfig.VIDEO_ID)
+                    }
+
+                    override fun onInitializationFailure(provider: YouTubePlayer.Provider, youTubeInitializationResult: YouTubeInitializationResult) {
+                    }
+                })
+
+        thumbnailView
+                .initialize(BuildConfig.API_KEY, object : YouTubeThumbnailView.OnInitializedListener {
+                    override fun onInitializationSuccess(thumbnailView: YouTubeThumbnailView?, loader: YouTubeThumbnailLoader?) {
+                        loader?.setVideo(BuildConfig.VIDEO_ID)
+                    }
+
+                    override fun onInitializationFailure(p0: YouTubeThumbnailView?, p1: YouTubeInitializationResult?) {
+                    }
+                })
     }
 }
